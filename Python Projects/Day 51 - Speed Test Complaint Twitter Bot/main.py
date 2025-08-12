1f4b8b006bd67db10dda8cb3d7709e89a1ab1f79
Eduard Mihaita <eduard.mihaita@aoblabs.ro>
2025-08-12 14:03:10 +0300

    Update main.py

--- Python Projects/Day 51 - Speed Test Complaint Twitter Bot/main.py
+++ Python Projects/Day 51 - Speed Test Complaint Twitter Bot/main.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-tw_username = "TEnderdj30298"
-tw_mail = "mihaita_e@yahoo.ro"
-tw_password = "bottest1243!"
+tw_username = ""
+tw_mail = ""
+tw_password = ""
 
 class InternetSpeedTwitterBot:
     def __init__(self):
